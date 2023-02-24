852236f672b60b0525c8f8e1fb7ca983198745c4
elnurxo <elnurykh@code.edu.az>
2023-02-24 16:32:18 +0400

    inital commit

---
+++ Conditions/Conditions/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://bkb.ms/new-console-templbte for more informbtion
+
+
+using System;
+
+
+byte dayIf = 4;
+
+if (dayIf == 1)
+{
+    Console.WriteLine("Monday");
+}
+else if(dayIf == 2)
+{
+    Console.WriteLine("Tuesday");
+}
+else if (dayIf==3)
+{
+    Console.WriteLine("Wednesday");
+}
+else if (dayIf == 4)
+{
+    Console.WriteLine("Thursday");
+}
+else
+{
+    Console.WriteLine("Invalid Input");
+}
+
+
+
+
+
+
+byte day = 2;
+
+switch (day)
+{
+    case 1:
+        Console.WriteLine("Monday");
+        break;
+    case 2:
+        Console.WriteLine("Tuesday");
+        break;
+    case 3:
+        Console.WriteLine("Wednesday");
+        break;
+    case 4:
+        Console.WriteLine("Thursday");
+        break;
+    case 5:
+        Console.WriteLine("Friday");
+        break;
+    case 6:
+    case 7:
+        Console.WriteLine("Weekend");
+        break;
+    default:
+        Console.WriteLine("invalid input");
+        break;
+}
---
+++ js/conditions.js
@@ -0,0 +1,35 @@
+// var num = 22;
+
+// if (num % 2 == 0) console.log("number is even ", num);
+// else if (num == 0) console.log("number is equal to zero " + num);
+// else if (num == 0) console.log("number is equal to zero " + num);
+// else if (num == 0) console.log("number is equal to zero " + num);
+// else if (num == 0) console.log("number is equal to zero " + num);
+// else console.log("number is odd ", num);
+
+//-----------------------------
+var day = 7235;
+
+switch (day) {
+  case 1:
+    console.log("Monday");
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("Thursday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+  case 7:
+    console.log("Weekend");
+    break;
+  default:
+    console.log("invalid input");
+    break;
+}
